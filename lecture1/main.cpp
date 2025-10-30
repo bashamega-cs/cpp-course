81e69e769594d0bac73da8b8c705a156e93e7ae0
Adam Naji <adam.webtools+1@gmail.com>
2025-10-30 13:52:04 +0300

    Lecture 1

---
+++ lecture1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //Version 1
+    cout << "My name is Adam Naji" << endl << "I am a full stack web developer" << endl << "I love open source";
+
+    //Version 2
+    cout << "My name is Adam Naji\nI am a full stack web developer\nI love open source" << endl;
+
+    //Version 3
+    cout << "My name is Adam Naji" << endl;
+    cout << "I am a full stack web developer" << endl;
+    cout << "I love open source" << endl;
+}
